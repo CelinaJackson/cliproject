15ef13dd1fa5c43879f68125b63ad19f1f88b52c
Celina Jackson <celinanjackson@gmail.com>
2020-08-07 00:32:13 +0000

    scraper

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -5,24 +5,27 @@ require 'open-uri'
 
 class ScrapePage
 
-  def scrape_list
-    site = "https://www.growerdirect.com/flower-varieties/json"
-    page = Nokogiri::HTML(open(site))
-
-    name = page.css("h3.related-title a").map do |flower| 
+ def scrape_list
+site = "https://www.growerdirect.com/flower-varieties/json"
+page = Nokogiri::HTML(open(site))
+  name = page.css("h3.related-title a").map do |flower| 
     flower.text
-    end
-  end 
-
-
-  def self.scrape_info 
-    site = "https://www.growerdirect.com/flower-varieties/json"
-    page = Nokogiri::HTML(open(site))
-    
-    flower_info = page.css("div.related-summary p").map do |info| 
-    info.text
-    end
   end
 
-end  
-   
+  name.each.with_index(1) do |value, index|
+    puts "#{index}: #{value}"
+  end
+end 
+
+def scrape_info
+site = "https://www.growerdirect.com/flower-varieties/json"
+page = Nokogiri::HTML(open(site))
+ flower_info = page.css("div.related-summary p").map do |info| 
+    info.text
+ end
+
+ flower_info.each.with_index(1) do |value, index|
+    puts "#{index}: #{value}"
+ end
+end
+end
